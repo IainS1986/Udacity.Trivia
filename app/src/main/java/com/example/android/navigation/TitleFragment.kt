911cba9646c207f4abf0c3dd1b47510edd349f12
Iain <i_stanford@hotmail.com>
2019-12-15 20:11:30 +0000

    Lesson 9
    Navigation graph addition + first navigation step

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -18,6 +20,12 @@ class TitleFragment : Fragment() {
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+
+//        binding.playButton.setOnClickListener {
+//            it.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+//        }
+        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+
         return binding.root
     }
 }
